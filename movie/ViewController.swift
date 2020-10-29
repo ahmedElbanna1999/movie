6f6381669fb80b5fdc88fd5890f2eef0976a2212
ahmed mamdouh <aelbanna930@gmail.com>
2020-10-30 01:58:04 +0200

    Initial Commit

---
+++ movie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  movie
+//
+//  Created by rivile on 10/30/20.
+//  Copyright © 2020 rivile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
